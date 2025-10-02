3b257307c05abd669ceb282b579d7ece2668d558
Barry Schwartz <150643+chemoelectric@users.noreply.github.com>
2025-10-02 18:01:43 -0500

    A bunch of things used by gnatcoll-bindings.

--- shared/basic.zsh
+++ shared/basic.zsh
@@ -13,6 +13,9 @@ fi
 
 tar=( tar --format=posix )
 bail_out=( exit 1 )
+if [[ x${check_arguments} = x ]]; then
+    check_arguments=( check )
+fi
 
 abs_src_tarball_dir=`realpath "${PWD}/src_tarballs"`
 abs_bin_tarball_dir=`realpath "${PWD}/bin_tarballs"`
@@ -48,35 +51,31 @@ mkdir -p "${abs_srcdirs_dir}"
 rm -R -f "${abs_srcdirs_dir}/${packname}" || ${bail_out}
 ${tar} -f "${src_tarball}" -C "${abs_srcdirs_dir}" -x || ${bail_out}
 mkdir -p "${abs_builddir}" || ${bail_out}
-if [[ `whence -w patch_function` == 'patch_function: function' ]]; then
+if [[ `whence -w patch_function` = 'patch_function: function' ]]; then
     (
-	cd "${abs_srcdir}" || ${bail_out}
-	patch_function || ${bail_out}
+        cd "${abs_srcdir}" || ${bail_out}
+        patch_function || ${bail_out}
     ) || ${bail_out}
 fi
 (
     cd "${abs_builddir}" || ${bail_out}
-    local -a default_configure_arguments
+    default_configure_arguments=( )
     if [[ "${ban_prefix}" != yes ]]; then
-	default_configure_arguments=(
-	    "${(@)default_configure_arguments}"
-	    --prefix=/usr/local
-	)
+        default_configure_arguments+=( --prefix=/usr/local )
     fi
     if [[ "${ban_enable_silent_rules}" != yes ]]; then
-	default_configure_arguments=(
-	    "${(@)default_configure_arguments}"
-	    --enable-silent-rules="${silent_rules}"
-	)
+        default_configure_arguments+=(
+            --enable-silent-rules="${silent_rules}"
+        )
     fi
     env TARGETED_HOST="${_targeted_host}" \
-	"${(@)environment_variables}" \
-	"${abs_srcdir}"/configure \
-	"${(@)default_configure_arguments}" \
-	"${(@)configure_arguments}" || ${bail_out}
+        "${(@)environment_variables}" \
+        "${abs_srcdir}"/configure \
+        "${(@)default_configure_arguments}" \
+        "${(@)configure_arguments}" || ${bail_out}
     make -j"${jobs}" "${(@)make_arguments}" || ${bail_out}
     if [[ "${_check}" != "no" ]] && [[ "${_check}" != "false" ]]; then
-       make -j"${check_jobs}" check || ${bail_out}
+       make -j"${check_jobs}" "${(@)check_arguments}" || ${bail_out}
     fi
     make install DESTDIR="${abs_destdir}" || ${bail_out}
 )
